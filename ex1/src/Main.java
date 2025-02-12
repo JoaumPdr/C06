e3040693479a9c3f030d0e9745fd75f0f3b63df3
jp <161966976+JoaumPdr@users.noreply.github.com>
2025-02-11 23:09:22 -0300

    Add files via upload

---
+++ ex1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int total = lanche1 + lanche2 + lanche3;
+
+        float media = total/3;
+        System.out.printf("Total de lanches: " + total);
+        System.out.printf("Media dos lanches: " + media);
+
+    }
+}
